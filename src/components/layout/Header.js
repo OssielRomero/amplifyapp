24102b79a031843a58df401835836f36716846d0
ossielromero <ossiel@riseup.net>
2019-10-01 21:51:14 -0500

    Todo List Update
    Axios, UUID, React-Router-Dom added, Dynamic styling

---
+++ src/components/layout/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header style={headerStyle}>
+      <h1>AmplifyApp</h1>
+      <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
+    </header>
+  )
+}
+
+const headerStyle = {
+  background: '#ff9900',
+  color: '#fff',
+  textAlign: 'center',
+  padding: '10px'
+}
+
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none'
+}
+
+export default Header;
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>Hello World</p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
